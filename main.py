944dddd95ffd82c4abf0b730474b6a0bd25ccf65
Ziyan Zhang <ziyanzhang@Ziyans-MacBook-Air.local>
2025-07-22 13:23:28 -0700

    Crop 60% of model's face

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ from PIL import Image
 import numpy as np
 import os
 
-image_path = 'DoubleTakeDoubleTakeFullSizeNotchedCapSleeveKnitTop/DoubleTakeDoubleTakeFullSizeNotchedCapSleeveKnitTop0.jpg' #upload image
+image_path = input("Enter image path: ") #upload image
 image = cv2.imread(image_path) # use opencv to read image (in BGR format)
+
+# check if image could be found
+if image is None:
+    print(f"Failed to read image, please check if the path is correct: {image_path}")
+    exit() # program end
+
 image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # MTCNN required RGB, convert BGR to RGB format
 
 detector = MTCNN() # Initialize MTCNN detector
@@ -16,16 +22,29 @@ faces = detector.detect_faces(image_rgb)
 if not faces:
     print("No face detected")
 else:
+    # find model's face
     def area(face):
         x, y, w, h = face['box']
         return w * h
     main_face = max(faces, key=area)
+
+    # x, y are the coordinates of the top left corner of face box, w = wideth, h = height
     x, y, w, h = main_face['box']
     x, y = max(0, x), max(0, y) # ensure there's no negative coordinate
 
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2) # draw frame
-
+    """
+    # draw frame
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
     # save result
     output_path = 'output_mainface.jpg'
     cv2.imwrite(output_path, image)
     print(f"Detected done, face frame saved: {output_path}")
+    """
+
+    # Crop 60% of model's face
+    crop_top = int(y + 0.6 * h)
+    crop_img = image[crop_top:, :]
+    cropped_output_path = 'output_cropped.jpg'
+    # save result
+    cv2.imwrite(cropped_output_path, crop_img)
+    print(f"Cropped image saved: {cropped_output_path}")
